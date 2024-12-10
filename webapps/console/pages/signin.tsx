eca41e53c92bb42f1563fd0c9ee0dbc2aeb120ea
Pedro Lopes <pedro.lopes@gympass.com>
2024-12-10 03:10:25 -0300

    feat: add generic oidc sso option

--- webapps/console/pages/signin.tsx
+++ webapps/console/pages/signin.tsx
@@ -4,12 +4,12 @@ import { Button, Input } from "antd";
 import { useAppConfig } from "../lib/context";
 import { AlertTriangle } from "lucide-react";
 import Link from "next/link";
-import { GithubOutlined } from "@ant-design/icons";
+import { GithubOutlined, KeyOutlined } from "@ant-design/icons";
 import React, { useState } from "react";
 import { feedbackError } from "../lib/ui";
 import { useRouter } from "next/router";
 import { branding } from "../lib/branding";
-import { credentialsLoginEnabled, githubLoginEnabled } from "../lib/nextauth.config";
+import { credentialsLoginEnabled, githubLoginEnabled, oidcLoginConfig } from "../lib/nextauth.config";
 import { useQuery } from "@tanstack/react-query";
 
 function JitsuLogo() {
@@ -92,7 +92,34 @@ function GitHubSignIn() {
   );
 }
 
-const NextAuthSignInPage = ({ csrfToken, providers: { github, credentials } }) => {
+function OIDCSignIn() {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  return (
+    <div className="space-y-4">
+      <Button
+        className="w-full"
+        icon={<KeyOutlined />}
+        loading={loading}
+        onClick={async () => {
+          try {
+            setLoading(true);
+            await signIn("oidc");
+            await router.push("/");
+          } catch (e: any) {
+            feedbackError("Failed to sign in with SSO provider", e);
+          } finally {
+            setLoading(false);
+          }
+        }}
+      >
+        Sign in with SSO
+      </Button>
+    </div>
+  );
+}
+
+const NextAuthSignInPage = ({ csrfToken, providers: { github, oidc, credentials } }) => {
   const router = useRouter();
   const nextAuthSession = useSession();
   const app = useAppConfig();
@@ -119,8 +146,9 @@ const NextAuthSignInPage = ({ csrfToken, providers: { github, credentials } }) =
       </div>
       <div>
         {credentials.enabled && <CredentialsForm />}
-        {credentials.enabled && github.enabled && <hr className="my-8" />}
+        {credentials.enabled && (github.enabled || oidc.enabled) && <hr className="my-8" />}
         {github.enabled && <GitHubSignIn />}
+        {oidc.enabled && <OIDCSignIn />}
       </div>
       {router.query.error && (
         <div className="text-error">
@@ -154,6 +182,9 @@ export async function getServerSideProps(context) {
         },
         github: {
           enabled: githubLoginEnabled,
+        },
+        oidc: {
+          enabled: !!oidcLoginConfig,
         },
       },
       publicPage: true,
